86c3205944c2ae17bfe64a82ee68634d06fdda38
colinslda <colin.soldati@icloud.com>
2025-02-09 12:18:24 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'dacapo-cache-v1';
+const urlsToCache = [
+  './',
+  './login.html',
+  './signup.html',
+  './main.html',
+  './app.js',
+  './style.css',
+  './manifest.json'
+  // Ajoute ici d'autres ressources (icônes, images, etc.)
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
